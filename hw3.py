0dd1e060ae25d0a136981b07ab4359579392c4fd
Gökay Gaş <gas15@itu.edu.tr>
2018-12-08 12:27:05 +0300

    Input points are fixed and triangulation is implemented.

--- hw3.py
+++ hw3.py
@@ -129,8 +129,106 @@ class Window(QtWidgets.QMainWindow):
 	def save_image(self):
 		cv2.imwrite("./output-image.png", self.outputImg)
 
+	# Check if a point is inside a rectangle
+	def rect_contains(self, rect, point) :
+		if point[0] < rect[0] :
+			return False
+		elif point[1] < rect[1] :
+			return False
+		elif point[0] > rect[2] :
+			return False
+		elif point[1] > rect[3] :
+			return False
+		return True
+
+	# Draw a point
+	def draw_point(self, img, p, color ) :
+		cv2.circle( img, p, 3, color, cv2.FILLED, cv2.LINE_AA, 0 )
+	
+	
+	# Draw delaunay triangles
+	def draw_delaunay(self, img, subdiv, delaunay_color) :
+	
+		triangleList = subdiv.getTriangleList()
+		size = img.shape
+		r = (0, 0, size[1], size[0])
+	
+		for t in triangleList :
+		
+			pt1 = (t[0], t[1])
+			pt2 = (t[2], t[3])
+			pt3 = (t[4], t[5])
+	
+			if self.rect_contains(r, pt1) and self.rect_contains(r, pt2) and self.rect_contains(r, pt3) :
+			
+				cv2.line(img, pt1, pt2, delaunay_color, 1, cv2.LINE_AA, 0)
+				cv2.line(img, pt2, pt3, delaunay_color, 1, cv2.LINE_AA, 0)
+				cv2.line(img, pt3, pt1, delaunay_color, 1, cv2.LINE_AA, 0)
+ 
+
 	def create_triangulation(self):
-		return NotImplementedError
+		copyOfImg1 = self.Img.copy()
+
+		inputSize = self.Img.shape
+		targetSize = self.Img2.shape
+
+		rect1 = (0,0,inputSize[1], inputSize[0])
+		rect2 = (0,0,targetSize[1], targetSize[0])
+
+		subdiv1 = cv2.Subdiv2D(rect1)
+		subdiv2 = cv2.Subdiv2D(rect2)
+
+		points1 = []
+		points2 = []
+
+		with open("inputPoints.txt") as file:
+			for line in file:
+				x, y = line.split(',')
+				points1.append((int(x), int(y)))
+		
+		with open("targetPoints.txt") as file:
+			for line in file:
+				x, y = line.split(',')
+				points2.append((int(x), int(y)))
+
+		for point in points1:
+			subdiv1.insert(point)
+		
+		for point in points2:
+			subdiv2.insert(point)
+
+		self.draw_delaunay(self.Img, subdiv1, (255, 255, 255))
+		self.draw_delaunay(self.Img2, subdiv2, (255, 255, 255))
+		self.draw_delaunay(copyOfImg1, subdiv2, (255, 255, 255))
+
+		for point in points1:
+			self.draw_point(self.Img, point, (0, 255, 255))
+		
+		for point in points2:
+			self.draw_point(self.Img2, point, (0, 255, 255))
+		
+		for point in points2:
+			self.draw_point(copyOfImg1, point, (0, 255, 255))
+
+		R, C, B = self.Img.shape
+		qImg = QtGui.QImage(self.Img.data, C, R, 3 * C, QtGui.QImage.Format_RGB888).rgbSwapped()
+		pix = QtGui.QPixmap(qImg)
+		self.label.setPixmap(pix)
+
+		R, C, B = self.Img2.shape
+		qImg2 = QtGui.QImage(self.Img2.data, C, R, 3 * C, QtGui.QImage.Format_RGB888).rgbSwapped()
+		pix2 = QtGui.QPixmap(qImg2)
+		self.label2.setPixmap(pix2)
+
+		R, C, B = copyOfImg1.shape
+		qImg3 = QtGui.QImage(copyOfImg1.data, C, R, 3 * C, QtGui.QImage.Format_RGB888).rgbSwapped()
+		
+		self.label3 = QtWidgets.QLabel(self.centralwidget)
+		pix3 = QtGui.QPixmap(qImg3)
+		self.label3.setPixmap(pix3)
+		self.label3.setAlignment(QtCore.Qt.AlignCenter)
+		self.label3.setStyleSheet("border:0px")
+		self.VerticalLayout3.addWidget(self.label3)
 
 	def morph(self):
 		return NotImplementedError
